da477823d285e78e5c916f3b089c626f38c0319c
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2019-04-13 21:36:12 +0200

    TCP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("couldn't bind to address");
+
+    for stream in listener.incoming() {
+        match stream.and_then(handle_client) {
+            Ok(_) => {
+                println!("client disconnected gracefully");
+            }
+            Err(e) => {
+                println!("client error: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+    println!("new client: {}", stream.peer_addr()?);
+    writeln!(stream, "hello client")?;
+
+    let mut buffer = [0; 10];
+    stream.read_exact(&mut buffer)?;
+    
+    if buffer == [65; 10] {
+        println!("yeh");
+    } else {
+        println!("oh");
+    }
+
+    Ok(())
 }
